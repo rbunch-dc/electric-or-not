034bcdf8a674ec261523378a415a632bbd029587
Robert Bunch <robert.bunch@tinroofsoftware.com>
2015-12-08 09:56:43 -0500

    Add mongo functionality

--- routes/index.js
+++ routes/index.js
@@ -4,27 +4,21 @@ var MongoClient = require('mongodb').MongoClient;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	MongoClient.connect('mongodb://localhost:27017/mongotest', function(error, db){
-		db.collection('teams').find().toArray(function(error, result){
-			console.log(result)
+	MongoClient.connect('mongodb://localhost:27017/electric', function(error, db){
+		//1. Get all pictures from the MongoDB
+		db.collection('photos').find().toArray(function(error, result){
+			console.log(result);
+			//5. Choose a random image from teh array and set it to a var
+			var rand = Math.floor(Math.random()*result.length);
+			//6. res.render the index view and send it the photo
+			res.render('index', { photo: result[rand] });
 		})
-	});
+	});	
 
-	//index page should load random picture/item
-	//1. Get all pictures from the MongoDB
 	//2. Get the current user from MongoDB vai req.ip
 	//3. Find which photos the current user has NOT voted on
 	//4. Load all those photos into an array.
-	//5.Choose a random image from teh array and set it to a var
-	//6. res.render the index view and send it the photo
 
-	var serverPhotos = [
-		{name: 'http://chadconway.pbworks.com/f/1253765817/news-electriccar1.jpg' },
-		{name: 'https://c2.staticflickr.com/2/1307/4700132636_cd67861c4b_b.jpg' }
-	];
-
-
-  res.render('index', { photos: serverPhotos });
 });
 
 router.get('/standings', function(req, res, next) {
@@ -34,12 +28,17 @@ router.get('/standings', function(req, res, next) {
 	res.render('index', {title: 'Standings'});
 });
 
-router.post('*', function(req,res,next){
-	//this will run for all posted pages
+router.post('/electric', function(req,res,next){
+	MongoClient.connect('mongodb://localhost:27017/electric', function(error, db){
+		db.collection('users').insertOne( {
+	      ip: req.ip,
+	      vote: "electric",
+	      image: " "
+		});
+		res.redirect('../');
+	});	
 });
+function vote(req, res){
+}
 
 module.exports = router;
-
-
-
-
