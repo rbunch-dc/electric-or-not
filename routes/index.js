992d46d1c83dec5be6937c7d790f9e6fd26efa38
Robert Bunch <robert.bunch@tinroofsoftware.com>
2015-12-09 01:12:24 -0500

    Update post function to be catch all, add standings page and functionallity, Add images

--- routes/index.js
+++ routes/index.js
@@ -2,22 +2,33 @@ var express = require('express');
 var router = express.Router();
 var MongoClient = require('mongodb').MongoClient;
 
+var mongoUrl = 'mongodb://localhost:27017/electric';
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	MongoClient.connect('mongodb://localhost:27017/electric', function(error, db){
+	MongoClient.connect(mongoUrl, function(error, db){
 		//1. Get all pictures from the MongoDB
-		db.collection('photos').find().toArray(function(error, result){
-			console.log(result);
-			//5. Choose a random image from teh array and set it to a var
-			var rand = Math.floor(Math.random()*result.length);
-			//6. res.render the index view and send it the photo
-			res.render('index', { photo: result[rand] });
-		})
-	});	
 
-	//2. Get the current user from MongoDB vai req.ip
-	//3. Find which photos the current user has NOT voted on
-	//4. Load all those photos into an array.
+		var currIP = req.ip;
+		db.collection('users').find({ip: currIP}).toArray(function(error, userResult){
+			//2. Get the current user from MongoDB vai req.ip
+
+			var photosVoted = [];
+			for(i=0;i<userResult.length;i++){
+				photosVoted.push(userResult[i]);
+			}
+
+			db.collection('photos').find({photo: {$nin: photosVoted}}).toArray(function(error, result){
+				var rand = Math.floor(Math.random() * result.length);
+				if(result.length == 0){
+					res.render('index', {photo: result[rand]});
+				}else{
+					res.render('index', {photo: result[rand]});
+				}
+
+			});
+		});	
+	});	
 
 });
 
@@ -25,20 +36,63 @@ router.get('/standings', function(req, res, next) {
 	//1. get ALL the photos
 	//2. Sort them by highest likes
 	//3. res.render the standings view and pass it the sorted photo array 
-	res.render('index', {title: 'Standings'});
+
+	MongoClient.connect(mongoUrl, function(error, db){
+		//1. Get all pictures from the MongoDB
+		db.collection('photos').find().toArray(function(error, result){
+			//Pass all votes
+
+			result.sort(function(p1, p2){
+				return (p2.totalVotes - p1.totalVotes);
+			});
+
+			res.render('standings', {photosStandings: result});
+
+		});	
+	});
 });
 
-router.post('/electric', function(req,res,next){
-	MongoClient.connect('mongodb://localhost:27017/electric', function(error, db){
-		db.collection('users').insertOne( {
-	      ip: req.ip,
-	      vote: "electric",
-	      image: " "
+router.post('*', function(req,res,next){
+	if(req.url == '/electric'){
+		var page = 'electric';
+	}else{
+		var page = 'poser';
+	}
+	MongoClient.connect(mongoUrl, function(error, db){
+		db.collection('photos').find({image: req.body.photo}).toArray(function(error, result){
+			var updateVotes = function(db, votes, callback) {
+				if(page=='electric'){var newVotes = votes+1;}
+				else{var newVotes = votes-1;}
+				
+			   db.collection('photos').updateOne(
+			      { "image" : req.body.photo },
+			      {
+			        $set: { "totalVotes": newVotes },
+			        $currentDate: { "lastModified": true }
+			      }, function(err, results) {
+			      // console.log(results);
+			      callback();
+			   });
+			};
+
+			MongoClient.connect(mongoUrl, function(error, db) {
+				updateVotes(db,result[0].totalVotes, function() {});
+			});
 		});
-		res.redirect('../');
+	});	
+
+	MongoClient.connect(mongoUrl, function(error, db){
+
+		db.collection('users').insertOne( {
+	    	ip: req.ip,
+	    	vote: page,
+	    	image: req.body.photo
+		});
+		res.redirect('/');
 	});	
 });
-function vote(req, res){
-}
+
 
 module.exports = router;
+
+
